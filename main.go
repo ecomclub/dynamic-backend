387d7be4a418e4fddcf5596930e91b015aaadfed
Leonardo Matos <leomp120894@gmail.com>
2018-01-22 14:56:58 -0200

    bind localhost and port from cli arg

--- main.go
+++ main.go
@@ -4,16 +4,21 @@ import (
   "log"
   "net/http"
   "os"
+  "fmt"
 )
 
 func main() {
   // get root directory from first command line argument
   // https://gobyexample.com/command-line-arguments
   root := os.Args[1]
+  // TCP port
+  port := os.Args[2]
+  // listen localhost only
+  bind := fmt.Sprintf("%s%s", "127.0.0.1", port)
 
   fs := http.FileServer(http.Dir(root))
   http.Handle("/", fs)
 
   log.Println("Listening...")
-  http.ListenAndServe(":3000", nil)
+  http.ListenAndServe(bind, nil)
 }
