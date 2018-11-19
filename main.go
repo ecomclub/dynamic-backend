dfe7f2f04ff9b2b6f353bdded968de72a3e03b4c
Leonardo Matos <leomp120894@gmail.com>
2018-11-19 21:30:45 -0200

    testing cookie headers

--- main.go
+++ main.go
@@ -92,8 +92,10 @@ func main() {
         if defaultLang != "" {
           http.SetCookie(w, &http.Cookie{Name: "Ecom.default_lang", Value: defaultLang, MaxAge: 60})
         }
-        http.SetCookie(w, &http.Cookie{Name: "Ecom." + r.URL.Path + ":resource", Value: resource, MaxAge: 30})
-        http.SetCookie(w, &http.Cookie{Name: "Ecom." + r.URL.Path + ":_id", Value: id, MaxAge: 30})
+        http.SetCookie(w, &http.Cookie{Name: "Ecom." + r.URL.Path + ":resource", Value: resource, MaxAge: 60})
+        http.SetCookie(w, &http.Cookie{Name: "Ecom." + r.URL.Path + ":_id", Value: id, MaxAge: 60})
+        http.SetCookie(w, &http.Cookie{Name: "Ecom.path:_id", Value: id, MaxAge: 60})
+        http.SetCookie(w, &http.Cookie{Name: "Ecom.path._id", Value: id, MaxAge: 30})
         // debug
         log.Println("&Resource ID")
         log.Println(id)
